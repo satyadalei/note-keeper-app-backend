edc90c09a71b27590cbfbf3f1f40fd4b60255bf6
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-04-13 14:22:05 +0530

    completing all authentication routes

---
+++ middlewares/fetchuser.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv').config();
+var jwt = require('jsonwebtoken');
+const fetchuser = (req,res,next)=>{
+    const token = req.header('auth-token'); // getting token from req header 
+    if (!token) {
+        // if token not found
+        return res.status(401).json({ "message": "You are not authorized to access data"});
+    }else{
+        try {
+            // token found and verify token
+            const userVerifiedData = jwt.verify(token,process.env.JWT_SIGN_SECRET); // returns basic datas from token liek --> { id: '64370b8bbd09ea8ff4495a5d', iat: 1681371861 }
+            req.userId = userVerifiedData.id; // adding user id to req params and passing to next 
+            next();  
+        } catch (error) {
+            //if token not verified
+            res.status(401).send("You are not authorized to acces");
+        }
+    }
+}
+module.exports = fetchuser;
--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const fetchUser = require('../middlewares/fetchuser')
 
-router.post("/", [
+//::ROUTE:1:::++++++++++++++++++++++++ CREATE A USER ++++++++++++++++++++++++
+router.post("/createuser", [
    body('email', "Enter a valid email").isEmail(),
    body('password', "Enter correct password").isLength({ min: 5 })
 ],
@@ -37,10 +39,8 @@ router.post("/", [
                const data = {
                   id : user.id
                }
-               console.log(user.id);
-               console.log(process.env.JWT_SIGN_SECRET);
                const token = jwt.sign(data,process.env.JWT_SIGN_SECRET)
-               res.json({ user, token });
+               res.json({token});
             }
          } catch (error) {
             console.log(error);
@@ -48,5 +48,55 @@ router.post("/", [
          }
       }
    })
-
+//::ROUTE:2:::++++++++++++++++++++++++ AUTHENTICATE A USER +++++++++++++++++++++++++++++++++
+router.post("/login",[
+   // check valid email or empty password
+   body('email', "Enter a valid email").isEmail(),
+   body('password', "Password can not be empty").exists()
+   ],
+   async (req,res)=>{
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+      }else{
+          const {email,password} = req.body;
+          try {
+            //--first find that user
+            const user = await User.findOne({email});
+            if (!user) {
+               // --- user does not exit
+               res.status(400).json({"Message":"Please login with correct credentials"})
+            }else{
+               //---user exists
+               const validUser =await bcrypt.compare(password,user.password); // compare user password with saved password
+               if(!validUser) {
+                  res.send("not a Valid user"); //password did not matched
+               }else{
+                  //password matched
+                  const data = {
+                     id : user.id
+                  }
+                  const token = jwt.sign(data,process.env.JWT_SIGN_SECRET)
+                  res.json({token});
+               }
+            }
+          } catch (error) {
+            //nothing goes right then catch error
+            console.log(error);
+            return res.status(500).json({ "message": "Something gone wrong" });
+          }
+      }
+   }
+)
+//::ROUTE:3:::+++++++++++++++++++++++::Get user after logIn :::++++++++++++++++++++++++++++
+router.post("/getuser",fetchUser,async (req,res)=>{
+   try {
+     const userId = req.userId ;// stores only id of user
+     const user = await User.findOne({id:userId}).select("-password");//gives user credentias without password
+      res.send({user});
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({ "message": "Something gone wrong" });
+   }
+})
 module.exports = router;
